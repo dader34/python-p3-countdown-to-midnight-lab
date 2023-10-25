9f7a30a66fbceddf5f364ff7ffe1bd2f8e2f02e8
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-25 16:11:34 -0700

    Passed all tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import time
+def countdown(f):
+    for i in range(f,0,-1):
+        print(f'{i} SECOND(S)!')
+
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(f):
+    for i in range(f,0,-1):
+        print(f'{i} SECOND(S)!')
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
